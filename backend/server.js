82e24f616e2c27f7ca31eac85c37b838f58273db
SupanutT <supanut.tangsin@gmail.com>
2024-01-16 17:20:33 +0700

    config : basic backend | docker

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const FetchJSONApiRoute = require('./Route/FetchJSONApiRoute');
+
+const app = express();
+
+app.listen(80, () => console.log('Listening to PORT 80'));
+
+app.use(cors());
+app.use('/', FetchJSONApiRoute);
