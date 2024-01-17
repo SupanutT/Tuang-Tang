3970dd6d0f90e135ae24e8046d650aaea3131efd
SupanutT <supanut.tangsin@gmail.com>
2024-01-17 16:28:39 +0700

    init : mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const FetchJSONApiRoute = require('./Route/FetchJSONApiRoute');
+const mongoose = require('mongoose');
+const FetchJSONApiRoute = require('./route/FetchJSONApiRoute');
 
 const app = express();
 
-app.listen(80, () => console.log('Listening to PORT 800'));
+mongoose.connect('mongodb://127.0.0.1:27017/tuang-tang', {});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+app.listen(80, () => console.log('Listening to PORT 80'));
 
 app.use(cors());
 app.use('/', FetchJSONApiRoute);
