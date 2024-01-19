3cb894e094065c7a1fd2a1204795d18c0a6e2242
SupanutT <107112702+SupanutT@users.noreply.github.com>
2024-01-20 00:44:05 +0700

    Delete backend directory

--- backend/Route/FetchJSONApiRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const JSONDataFetch = require('../Controller/FetchJSONApiController');
-const router = express.Router();
-
-router.get('/fetch-api-data', JSONDataFetch.FetchJSONAPIData); // API endpoint
-
-module.exports = router;
