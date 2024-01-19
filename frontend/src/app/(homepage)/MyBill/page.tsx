9bdc7ef415576dc72e02fb0f926e9fe6b238d35e
SupanutT <supanut.tangsin@gmail.com>
2024-01-19 23:07:46 +0700

    test : CI/CD for frontend

--- frontend/src/app/(homepage)/MyBill/page.tsx
+++ frontend/src/app/(homepage)/MyBill/page.tsx
@@ -1,10 +1,11 @@
 import BillList from "../../components/BiilList";
 import AddBill from "@/app/components/AddBill";
 
-export default function MyBill(){
+export default function MyBill() {
     return (
         <main className="mt-[50px]">
-            <BillList who="my"/>
+            <h1>CI/CD FROM GITHUB ACTIONS</h1>
+            <BillList who="my" />
             <AddBill />
         </main>
     );
