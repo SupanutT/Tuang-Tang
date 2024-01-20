ef718421954942cf8e96d468cb96c0d6e62bc078
SupanutT <107112702+SupanutT@users.noreply.github.com>
2024-01-20 23:21:58 +0700

    Delete frontend directory

--- frontend/src/app/(homepage)/mybill/[nid]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function MyBillDetailPage( {params}: { params: {nid:string}}){
-    return (
-        <main className="mt-[80px] px-[10px]">
-            <div className="text-md">
-                MyBill ID {params.nid}
-            </div>
-        </main>
-    );
-}
--- frontend/src/app/(homepage)/otherbill/[nid]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function OtherBillDetailPage( {params}: { params: {nid:string}}){
-    return (
-        <main className="mt-[80px] px-[10px]">
-            <div className="text-md">
-                OtherBill ID {params.nid}
-            </div>
-        </main>
-    );
-}
