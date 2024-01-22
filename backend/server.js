b203e8857321384d998d166847e12c16c456f5ed
SupanutT <supanut.tangsin@gmail.com>
2024-01-23 00:38:49 +0700

    fix : mongo connection

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const MongoStore = require('connect-mongo');
 
-const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/tuang-tang';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl, {});
 const db = mongoose.connection;
