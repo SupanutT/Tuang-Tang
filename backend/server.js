049ae41cd34aa5babec40f7ae768bc30294a7e6c
SupanutT <supanut.tangsin@gmail.com>
2024-01-23 03:28:10 +0700

    fix : cors.origin

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000']
+    origin: ['http://www.tuang-tang.site']
 }));
 
 const jwtOptions = {
