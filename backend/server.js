58631f329fe68c6725de7c2a54966c3b9aa03937
SupanutT <supanut.tangsin@gmail.com>
2024-01-23 03:23:17 +0700

    test : JWT

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,10 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000']
+}));
 
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
