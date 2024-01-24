266c5c9bb1269eb02044e9e3895d8c20fb9049e8
SupanutT <107112702+SupanutT@users.noreply.github.com>
2024-01-24 16:31:45 +0700

    Delete frontend directory

--- frontend/src/libs/getBill.tsx
+++
@@ -1,9 +0,0 @@
-export default async function getBill( { params }: { params: {nid: string}}){
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/bills/${params.nid}`)
-
-    if(!response.ok){
-        throw new Error("Failed to fetch bill")
-    }
-
-    return await response.json()
-}
--- frontend/src/libs/getBills.tsx
+++
@@ -1,9 +0,0 @@
-export default async function getBills(){
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/api/bills`)
-
-    if(!response.ok){
-        throw new Error("Failed to fetch bills")
-    }
-
-    return await response.json()
-}
