36b2a443f32c61e465fcc9b507b5f604760ae6c8
Jaiiaj21 <jaijai211075@gmail.com>
2024-01-24 16:47:09 +0700

    frontend: edit billData recieve wrong data

--- frontend/src/libs/getBills.tsx
+++ frontend/src/libs/getBills.tsx
@@ -1,5 +1,5 @@
 export default async function getBills(){
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/api/bills`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/bills`)
 
     if(!response.ok){
         throw new Error("Failed to fetch bills")
