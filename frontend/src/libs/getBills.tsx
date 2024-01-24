68c7214b07bcb3240e25e6c324af0ffb86af74a7
Jaiiaj21 <jaijai211075@gmail.com>
2024-01-24 16:20:39 +0700

    fix: edit env file

--- frontend/src/libs/getBills.tsx
+++ frontend/src/libs/getBills.tsx
@@ -1,5 +1,5 @@
 export default async function getBills(){
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_IMG}/api/bills`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/api/bills`)
 
     if(!response.ok){
         throw new Error("Failed to fetch bills")
