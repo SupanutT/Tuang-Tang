c3af57702ba39c3d0c6a223336f69e3831b73615
SupanutT <supanut.tangsin@gmail.com>
2024-01-31 16:32:55 +0700

    backend : feat refresh token

---
+++ backend/models/refreshToken.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = require('./user');
+
+const refreshTokenSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    refreshToken: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('RefreshToken', refreshTokenSchema);
