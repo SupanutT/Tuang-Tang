8a0e5e3330b1bdb294e682d6d9557b2a1691c2c3
SupanutT <supanut.tangsin@gmail.com>
2024-02-01 02:35:37 +0700

    add : refreshTokenRoute

--- backend/models/refreshToken.js
+++ backend/models/refreshToken.js
@@ -13,6 +13,11 @@ const refreshTokenSchema = new Schema({
         type: String,
         required: true
     },
+    status: {
+        type: string,
+        enum: ['issued', 'claimed', 'terminated'],
+        required: true
+    }
 });
 
 module.exports = mongoose.model('RefreshToken', refreshTokenSchema);
